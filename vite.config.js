4fa5b3c0e999fb3fe23bd2960d25172be830c505
Daniel Ahn <et3rnal.d@gmail.com>
2022-06-28 17:22:56 -0400

    bug: fix path for Components in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'url'
-import Components from 'unplugin-vue-components'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
