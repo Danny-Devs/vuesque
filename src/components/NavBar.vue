a241275b82c064c58f6067ae06d49c8f049da4c4
Daniel Ahn <et3rnal.d@gmail.com>
2022-06-29 23:46:42 -0400

    feat: add skeleton NavBar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="py-2 sm:py-6">
+    <div>logo/logotype here</div>
+
+    <ul>
+      <li>Link 1</li>
+      <li>Link 2</li>
+      <li>Link 3</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,18 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    // add the object below for your other routes! That's why I didn't delete the code below.
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
+  ],
 })
 
 export default router
